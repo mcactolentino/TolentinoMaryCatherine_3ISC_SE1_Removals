912a2fc93a64e8966733d2f95de090ec965a2ef1
mcactolentino <60631811+mcactolentino@users.noreply.github.com>
2021-11-25 14:10:14 +0800

    added script7.js

---
+++ script7.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name, breed, color){
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+    }
+}
+
+var doggie = new Dog('zet', 'chowchow', 'black');
+
+console.log(doggie);
