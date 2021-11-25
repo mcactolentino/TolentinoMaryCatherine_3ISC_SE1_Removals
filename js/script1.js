a27f054a008217ac5bb6b1f9113b2825a1895124
mcactolentino <60631811+mcactolentino@users.noreply.github.com>
2021-11-25 13:16:37 +0800

    added script1.js

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+let tintin ={
+    breed: 'Ragdoll',
+    color: 'Calico',
+    age: 1,
+    gender: 'female',
+
+    speak(){
+        return "Meow..."
+    },
+
+    eat(){
+        return "I love to eat tuna"
+    }
+};
+
+console.log(tintin.speak());
+console.log(tintin.eat())
