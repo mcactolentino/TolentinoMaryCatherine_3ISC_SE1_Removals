1d20f314b78e425d2a547904821d7581a5cccb34
mcactolentino <60631811+mcactolentino@users.noreply.github.com>
2021-11-25 14:06:35 +0800

    added script1.js

---
+++ script1.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+let tintin ={
+    breed: 'Ragdoll',
+    color: 'Calico',
+    age: 1,
+    gender: 'female',
+
+    speak(){
+        return "Meow..."
+    },
+
+    eat(){
+        return "I love to eat tuna"
+    }
+};
+
+console.log(tintin.speak());
+=======
+let tintin ={
+    breed: 'Ragdoll',
+    color: 'Calico',
+    age: 1,
+    gender: 'female',
+
+    speak(){
+        return "Meow..."
+    },
+
+    eat(){
+        return "I love to eat tuna"
+    }
+};
+
+console.log(tintin.speak());
+>>>>>>> a27f054a008217ac5bb6b1f9113b2825a1895124
+console.log(tintin.eat())
